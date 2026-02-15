7a3924d54b7c9911dab96d506499e7b90d91abec
Christina Ceran <christinaceran@christinas-air.lan>
2026-02-15 15:02:47 -0700

    Add conditional logic and age validation

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print ("Hello Christina. You're officially coding!") 
+name = input ("What is your name? ")
+print ("Hello", name)
+age = int(input ("How old are you? "))
+
+
+if age < 0: 
+    print ("That is not a valid age.")
+    raise SystemExit
+elif age < 18:
+    print ("You are a minor.")
+elif age < 30:
+    print ("You are in your twenties.")
+elif age < 50:
+    print ("You are an adult.")
+else: 
+    print ("You are a senior.")
+
+
